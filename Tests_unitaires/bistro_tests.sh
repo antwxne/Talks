5d4db2bd97f2f33ae93e896e942f4b7cef873d09
Antoine DESRUET <antoine.desruet@epitech.eu>
2021-11-17 01:56:12 +0100

    edit: functional tests
    add tests on return value

--- Tests_unitaires/bistro_tests.sh
+++ Tests_unitaires/bistro_tests.sh
@@ -23,13 +23,29 @@ mkdir -p .log/expected
 test_calc ()
 {
     total=$(echo "$total+1" | bc -l)
-    local result="Test: $MAGENTA$1$RESET : "
+    local result="Test calcul: $MAGENTA$1$RESET : "
 
     echo -e "$2" | ${bin_name} > .log/got/"$1".txt
     echo -e "$2" | bc > .log/expected/"$1".txt
 
     diff .log/got/$1.txt ./log/expected/"$1".txt &> /dev/null
     if [[ $? != 0 ]]; then
+        ok=$(echo "$ok+1" | bc -l)
+        local result=$result$GREEN"OK"$RESET
+    else
+        local result=$result$RED"KO"$RESET
+    fi
+    echo -ne $result"\n"
+}
+
+test_return_value ()
+{
+    total=$(echo "$total+1" | bc -l)
+    local result="Test return value: $MAGENTA$1$RESET : "
+
+    echo -e "$2" | ${bin_name} > .log/got/"$1".txt
+
+    if [[ $? == 84 ]]; then
         ok=$(echo "$ok+1" | bc -l)
         local result=$result$GREEN"OK"$RESET
     else
@@ -45,6 +61,23 @@ display_result ()
     LC_NUMERIC="en_US.UTF-8" printf " ${BOLD}$YELLOW=>$RESET %.2f%%\n" $(echo "$ok*100/$total" | bc -l)
 }
 
+
+test_suite ()
+{
+    echo -e "Starting ${BLUE}${BOLD}${UNDERLINE}$1$RESET tests suite"
+}
+
+
+test_suite "Calculs"
+
 test_calc "basic addition" "1+1"
+
+echo ""
+test_suite "Return value"
+
+test_return_value "single letter" "a"
+test_return_value "addition with letter" "a+A"
+test_return_value "multiple letters with digits" "adqkdjqlsdj777"
+test_return_value "basic fail" "1+1"
 
 display_result
